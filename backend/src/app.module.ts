581867822b786b2378f73e0d8728453557dd2409
vlldnt <vieilledent.adrien@gmail.com>
2025-11-21 14:19:50 +0100

    chore: deleting Event ---> neec ompany first

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthController } from './health.controller';
-import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
     PrismaModule,
     UsersModule,
     AuthModule,
-    EventsModule,
   ],
   controllers: [HealthController],
   providers: [],
